ee86388c275a378b2587c45cafe740ad3efcb193
Kunsiheir <nicholaslopezdiaz629@gmail.com>
2025-12-17 15:24:22 -0400

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -54,7 +54,7 @@ PACKAGES=(
 	yt-dlp minizip nautilus genymotion jre17-openjdk gnome-themes-extra
  	ffmpegthumbnailer tmux
 	# extra new
-	helix mutt fzf jre-openjdk blender
+	helix mutt fzf jre-openjdk
 	proton-vpn-gtk-app proton-vpn-cli
 )
 
